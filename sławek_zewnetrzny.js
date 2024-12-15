135ba0a8670da7b4e8ec05dc9b0e76b3d2c53cba
BartoszSlawek <144596247+BartoszSlawek@users.noreply.github.com>
2024-12-15 20:32:16 +0100

    Add files via upload

---
+++ sławek_zewnetrzny.js
@@ -0,0 +1,8 @@
+document.write(
+    "ostatnia modyfikacja strony"
+        .fontcolor("purple")    
+		.bold()
+        .fontsize(5)         
+        + "<br>"
+);
+document.write(document.lastModified);
